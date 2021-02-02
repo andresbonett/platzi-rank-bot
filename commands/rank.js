7a9b27dd5ecce0084230080b625f7d424e2faaf1
CarGDev <ingecarlos.gutierrez@gmail.com>
2021-02-01 19:17:06 -0600

    Modifying the platzi rank

--- commands/rank.js
+++ commands/rank.js
@@ -7,11 +7,21 @@ module.exports = bot => async msg => {
 
   const { status, error, data } = await fetchUserDataPlatzi(userPlatzi)
   if (error === 200) {
+    //console.log(data)
     const user = data.username
     const rank = data.platzi_rank
     const text = `🏁platzi rank: <b>@${user}</b> = <i>${rank}</i>`
+    const totalCarees = data.careers.length
+    const totalCont = data.contributions.length
+    const totalCurses = data.courses.length
+    const coursesInac = data.inactive_courses.length
+    console.log(totalCurses, totalCont, totalCarees, coursesInac)
     console.log(text)
     bot.sendMessage(chatId, text, { parse_mode: 'HTML' })
+    bot.sendMessage(chatId, `@${user} ha respondido ${data.answers} veces en Platzi`)
+    bot.sendMessage(chatId, `@${user} ha hecho ${totalCurses} cursos en total`)
+    bot.sendMessage(chatId, `@${user} ha hecho ${totalCont} aportaciones en total`)
+    bot.sendMessage(chatId, `@${user} ha terminado ${totalCarees} carreras en total`)
   } else {
     if (status == 'PrivateProfile') {
       bot.sendMessage(chatId, 'user is private!')
